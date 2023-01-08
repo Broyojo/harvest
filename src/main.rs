ca8a8eff80ce2fdd3e78845c8cd2d7e560ecbf4e
notrealn <56411504+notrealn@users.noreply.github.com>
2023-01-07 20:44:12 -0500

    tiles

--- src/main.rs
+++ src/main.rs
@@ -25,17 +25,60 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>, windows: Res<Windows>) {
     commands.spawn(Camera2dBundle::default());
 
-    let texture_handle = asset_server.load("player.png");
+    let window = windows.primary();
+
+    let player_texture = asset_server.load("player.png");
+    let floor_texture = asset_server.load("ohno.png");
+
+    let w = window.width() / 16.0;
+    let h = window.height() / 9.0;
+
+    let tiles = (0..9)
+        .map(|row| {
+            (0..16)
+                .map(|col| Tile {
+                    floor: commands
+                        .spawn(SpriteBundle {
+                            sprite: Sprite {
+                                custom_size: Some(Vec2 { x: w, y: h }),
+                                ..default()
+                            },
+                            texture: floor_texture.clone(),
+                            transform: Transform {
+                                translation: Vec3::new(
+                                    w * (col as f32 - 7.5),
+                                    h * (row - 4) as f32,
+                                    0.0,
+                                ),
+                                ..default()
+                            },
+                            ..default()
+                        })
+                        .id(),
+                    block: commands.spawn(SpriteBundle { ..default() }).id(),
+                })
+                .collect()
+        })
+        .collect();
+
+    commands.insert_resource(Game { tiles: tiles });
 
     commands
         .spawn(SpriteBundle {
-            texture: texture_handle,
-            transform: Transform::from_scale(Vec3::splat(1.0)),
+            sprite: Sprite {
+                custom_size: Some(Vec2 { x: w, y: h }),
+                ..default()
+            },
+            texture: player_texture,
+            transform: Transform {
+                translation: Vec2::ZERO.extend(1.0),
+                ..default()
+            },
             ..default()
         })
         .insert(Player)
         .insert(Velocity(Vec3::default()))
-        .insert(LinearDamping(2.0));
+        .insert(Damping(2.0));
 }
 
 fn player_movement(mut query: Query<&mut Velocity, With<Player>>, keys: Res<Input<KeyCode>>) {
@@ -59,7 +102,7 @@ fn player_movement(mut query: Query<&mut Velocity, With<Player>>, keys: Res<Inpu
     }
 }
 
-fn linear_damping_system(mut query: Query<(&mut Velocity, &LinearDamping)>) {
+fn damping_system(mut query: Query<(&mut Velocity, &Damping)>) {
     for (mut velocity, damping) in query.iter_mut() {
         if velocity.0.length() > BASICALLY_ZERO {
             velocity.0 /= damping.0;
@@ -73,17 +116,33 @@ fn velocity_system(mut query: Query<(&mut Transform, &Velocity)>) {
     }
 }
 
+// enum FloorType {
+//     Ground,
+// }
+
+// enum BlockType {
+//     Air,
+// }
+
 #[derive(Component)]
 struct Player;
+
+struct Tile {
+    floor: Entity,
+    block: Entity,
+}
 
 #[derive(Component)]
 struct Velocity(Vec3);
 
 #[derive(Component)]
-struct LinearDamping(f32);
+struct Damping(f32);
 
-#[derive(Component)]
-struct Tile;
+#[derive(Resource)]
+struct Game {
+    // [row][col]
+    tiles: Vec<Vec<Tile>>,
+}
 
 #[derive(Resource)]
 struct Settings;
@@ -95,7 +154,7 @@ impl Plugin for GamePlugin {
         app.add_startup_system(setup)
             .add_system(player_movement)
             .add_system(velocity_system)
-            .add_system(linear_damping_system);
+            .add_system(damping_system);
     }
 }
 
